8970dc39fd151e0d88d3fca9109841a0e50c1b8f
DATIOR.LEONARDO <leonardowederveiga@gmail.com>
2025-03-30 16:22:14 -0300

    Visual adaptado do carrosel, com uma pequena falha

--- src/app/componentes/componentesSmall/frases-carrosel/frases-carrosel.component.ts
+++ src/app/componentes/componentesSmall/frases-carrosel/frases-carrosel.component.ts
@@ -11,10 +11,10 @@ import { CommonModule } from '@angular/common';
 
 export class FrasesCarroselComponent {
   frases = [
-    { titulo: "Título 1", frase: "frase 1lsd asdsaodasoidsjad osajdoi sadoasmd asod aoidaoid masoid asmoid kkkkkkkkkkkkkkkkkkk", autor: "autor 1" },
-    { titulo: "Título 2", frase: "frase 2", autor: "autor 2" },
-    { titulo: "Título 3", frase: "frase 3", autor: "autor 3" },
-    { titulo: "Título 4", frase: "frase 4", autor: "autor 4" }
+    { titulo: "Título 1", frase: "O sucesso é a soma de pequenos esforços repetidos dia após dia, mesmo quando ninguém está olhando.", autor: "Robert Collier" },
+    { titulo: "Título 2", frase: "A vida não é medida pelo número de vezes que respiramos, mas pelos momentos que nos tiram o fôlego.", autor: "Maya Angelou" },
+    { titulo: "Título 3", frase: "A felicidade não está em outro lugar, mas sim na maneira como escolhemos enxergar e viver cada momento do presente.", autor: " Thich Nhat Hanh" },
+    { titulo: "Título 4", frase: "Se você não pode voar, corra. Se não pode correr, ande. Se não pode andar, rasteje. Mas, independentemente de tudo, continue em frente.", autor: "Luther King" }
   ];
 
   frasesGrupos: { titulo: string; frase: string; autor: string; }[][] = [];
