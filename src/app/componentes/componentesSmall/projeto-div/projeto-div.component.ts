5f777ea914c1679cf718e7f4c438376196d93c5e
DATIOR.LEONARDO <leonardowederveiga@gmail.com>
2025-04-13 22:26:28 -0300

    Adicionando animação no inicio do site

--- src/app/componentes/componentesSmall/projeto-div/projeto-div.component.ts
+++ src/app/componentes/componentesSmall/projeto-div/projeto-div.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import {  Component,
+  ElementRef,
+  Input,
+  ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-projeto-div',
@@ -10,4 +13,20 @@ export class ProjetoDivComponent {
   @Input() descricao_projeto: string = '';
   @Input() caminho_imagem: string = '';
   @Input() link_projeto: string = '#';
+
+  @ViewChild('card', { static: true }) cardRef!: ElementRef;
+  visivel = false;
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.visivel = true;
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 0.5 });
+
+    observer.observe(this.cardRef.nativeElement);
+  }
 }
