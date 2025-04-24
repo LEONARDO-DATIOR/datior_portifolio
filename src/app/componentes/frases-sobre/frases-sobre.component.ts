0db9cede2c23975eec77ec2b9e73d1ce03fc53da
DATIOR.LEONARDO <leonardowederveiga@gmail.com>
2025-04-23 22:09:03 -0300

    adicionando frases no sobre

---
+++ src/app/componentes/frases-sobre/frases-sobre.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FrasesCarroselComponent } from "../componentesSmall/frases-carrosel/frases-carrosel.component";
+
+@Component({
+  selector: 'app-frases-sobre',
+  imports: [FrasesCarroselComponent],
+  templateUrl: './frases-sobre.component.html',
+})
+export class FrasesSobreComponent {
+
+}
--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { InicioSobreComponent } from '../../componentes/inicio-sobre/inicio-sobre.component';
+import { FrasesCarroselComponent } from "../../componentes/componentesSmall/frases-carrosel/frases-carrosel.component";
+import { FrasesHomeComponent } from "../../componentes/frases-home/frases-home.component";
+import { FrasesSobreComponent } from "../../componentes/frases-sobre/frases-sobre.component";
 
 @Component({
   selector: 'app-sobre',
-  imports: [InicioSobreComponent],
+  imports: [InicioSobreComponent, FrasesSobreComponent],
   templateUrl: './sobre.component.html',
 })
 export class SobreComponent {
