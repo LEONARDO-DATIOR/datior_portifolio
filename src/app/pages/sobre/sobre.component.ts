a8a21b5c8057ea3d302b487bb9433483d40dd945
DATIOR.LEONARDO <leonardowederveiga@gmail.com>
2025-04-26 17:46:21 -0300

    finalização da construção beta para desktop na pagina sobre

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -3,10 +3,13 @@ import { InicioSobreComponent } from '../../componentes/inicio-sobre/inicio-sobr
 import { FrasesCarroselComponent } from "../../componentes/componentesSmall/frases-carrosel/frases-carrosel.component";
 import { FrasesHomeComponent } from "../../componentes/frases-home/frases-home.component";
 import { FrasesSobreComponent } from "../../componentes/frases-sobre/frases-sobre.component";
+import { TextoSobreComponent } from '../../componentes/texto-sobre/texto-sobre.component';
+import { ApresentacaoComponent } from "../../componentes/apresentacao/apresentacao.component";
+import { ContatoHomeComponent } from "../../componentes/contato-home/contato-home.component";
 
 @Component({
   selector: 'app-sobre',
-  imports: [InicioSobreComponent, FrasesSobreComponent],
+  imports: [InicioSobreComponent, FrasesSobreComponent, TextoSobreComponent, ApresentacaoComponent, ContatoHomeComponent],
   templateUrl: './sobre.component.html',
 })
 export class SobreComponent {
