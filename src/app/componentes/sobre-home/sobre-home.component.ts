ea5df5fe92cdb2ecfe58628acd3826c9e9cfd15e
LEONARDO-DATIOR <leonardowederveiga@gmail.com>
2025-07-12 22:47:39 -0300

    Realizado responsividade basica para mobile e tablet

--- src/app/componentes/sobre-home/sobre-home.component.ts
+++ src/app/componentes/sobre-home/sobre-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, NgZone} from '@angular/core';
 
 @Component({
   selector: 'app-sobre-home',
@@ -7,24 +7,39 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 
 export class SobreHomeComponent  {
-  @ViewChild('textoSobreHome', { static: true }) cardRef!: ElementRef;
-  visivel = false;
+  textoDigitado = '';
+  textoCompleto = `Jovem talento de 22 anos, formado em Análise e Desenvolvimento de Sistemas,
+com mais de três anos de experiência profissional na área de tecnologia.
+Atualmente ocupa o cargo de analista de infraestrutura na Localiza, pela DXC.`;
 
-  ngAfterViewInit() {
-    if (typeof IntersectionObserver !== 'undefined') {
-      const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            this.visivel = true;
-            observer.unobserve(entry.target);
+  @ViewChild('sectionRef', { static: true }) sectionRef!: ElementRef;
+  jaDigitou = false;
+
+  constructor(private ngZone: NgZone) {}
+
+  ngAfterViewInit(): void {
+    requestAnimationFrame(() => {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting && !this.jaDigitou) {
+            this.jaDigitou = true;
+            this.digitarTexto();
+            observer.disconnect();
           }
-        });
-      }, { threshold: 0.5 });
+        },
+        { threshold: 0.5 }
+      );
 
-      observer.observe(this.cardRef.nativeElement);
-    } else {
-      console.warn("IntersectionObserver não suportado");
-    }
+      observer.observe(this.sectionRef.nativeElement);
+    });
   }
 
+  digitarTexto(index = 0) {
+    if (index < this.textoCompleto.length) {
+      this.ngZone.run(() => {
+        this.textoDigitado += this.textoCompleto.charAt(index);
+      });
+      setTimeout(() => this.digitarTexto(index + 1), 25);
+    }
+  }
 }
