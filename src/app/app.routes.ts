a45b8d8a18ca598c6be2428f9ccf400c9bc52bd1
LEONARDO-DATIOR <leonardowederveiga@gmail.com>
2025-07-14 19:39:03 -0300

    Criando pagina de certificados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 import { NgModule } from '@angular/core';
 import { ContatoComponent} from './pages/contato/contato.component';
+import { CertificadosComponent } from './pages/certificados/certificados.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'Sobre', component: SobreComponent},
     {path: 'Contato', component: ContatoComponent},
+    {path: 'Certificados', component: CertificadosComponent}
     // {path: 'Certificados',}
     // {path: 'Contato',}
 ];
---
+++ src/app/pages/certificados/certificados.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CategoriaConhecimentoComponent } from '../../componentes/categoria-conhecimento/categoria-conhecimento.component';
+
+@Component({
+  selector: 'app-certificados',
+  imports: [CategoriaConhecimentoComponent],
+  templateUrl: './certificados.component.html',
+})
+export class CertificadosComponent {
+
+}
