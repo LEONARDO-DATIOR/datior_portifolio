a14987adff09051bb86ab781d1b61cba5daee0e0
LEONARDO-DATIOR <leonardowederveiga@gmail.com>
2025-07-16 19:13:04 -0300

    Adicionando mais sections em certificados

---
+++ src/app/componentes/principais-certificados/principais-certificados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principais-certificados',
+  imports: [],
+  templateUrl: './principais-certificados.component.html',
+})
+export class PrincipaisCertificadosComponent {
+
+}
--- src/app/pages/certificados/certificados.component.ts
+++ src/app/pages/certificados/certificados.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CategoriaConhecimentoComponent } from '../../componentes/categoria-conhecimento/categoria-conhecimento.component';
+import { PrincipaisCertificadosComponent } from '../../componentes/principais-certificados/principais-certificados.component';
 
 @Component({
   selector: 'app-certificados',
-  imports: [CategoriaConhecimentoComponent],
+  imports: [CategoriaConhecimentoComponent, PrincipaisCertificadosComponent],
   templateUrl: './certificados.component.html',
 })
 export class CertificadosComponent {
