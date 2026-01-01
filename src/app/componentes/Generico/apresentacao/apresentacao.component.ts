02145f4ea1959f4eaab8e19c7dbd0bd3c788f753
LEONARDO-DATIOR <leonardowederveiga@gmail.com>
2026-01-01 20:37:20 -0300

    Adicinando botao play para o video aprensentacao

--- src/app/componentes/Generico/apresentacao/apresentacao.component.ts
+++ src/app/componentes/Generico/apresentacao/apresentacao.component.ts
@@ -6,26 +6,5 @@ import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
   templateUrl: './apresentacao.component.html',
 })
 export class ApresentacaoComponent {
- 
-  @ViewChild('video', { static: true }) videoRef!: ElementRef;
-  visivel = false;
-
-  ngAfterViewInit() {
-    if (typeof IntersectionObserver !== 'undefined') {
-      const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            this.visivel = true;
-            observer.unobserve(entry.target);
-          }
-        });
-      }, { threshold: 0.5 });
-
-      observer.observe(this.videoRef.nativeElement);
-    } else {
-      console.warn("IntersectionObserver não suportado");
-    }
-  }
-
   
 }
