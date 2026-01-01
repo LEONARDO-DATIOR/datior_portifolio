66b6abddb99366fb607f0879c46ce850d1fa4b48
LEONARDO-DATIOR <leonardowederveiga@gmail.com>
2026-01-01 19:52:34 -0300

    Adicinando componente de projeto novo

--- src/app/componentes/componentesSmall/projeto-div/projeto-div.component.ts
+++ src/app/componentes/componentesSmall/projeto-div/projeto-div.component.ts
@@ -2,10 +2,11 @@ import {  Component,
   ElementRef,
   Input,
   ViewChild} from '@angular/core';
+import { BotaoPadraoComponent } from "../botao-padrao/botao-padrao.component.js";
 
 @Component({
   selector: 'app-projeto-div',
-  imports: [],
+  imports: [BotaoPadraoComponent],
   templateUrl: './projeto-div.component.html',
 })
 export class ProjetoDivComponent {
@@ -13,26 +14,5 @@ export class ProjetoDivComponent {
   @Input() descricao_projeto: string = '';
   @Input() caminho_imagem: string = '';
   @Input() link_projeto: string = '#';
-
-  @ViewChild('projeto', { static: true }) cardRef!: ElementRef;
-  visivel = false;
-
-  ngAfterViewInit() {
-    if (typeof IntersectionObserver !== 'undefined') {
-      const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            this.visivel = true;
-            observer.unobserve(entry.target);
-          }
-        });
-      }, { threshold: 0.5 });
-
-      observer.observe(this.cardRef.nativeElement);
-    } else {
-      console.warn("IntersectionObserver não suportado");
-    }
-  } 
-
-
+  
 }
