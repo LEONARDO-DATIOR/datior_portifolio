65a2ef6e308967d0b0c0be53cf708c4bd9937f92
DUCA.LEONARDO <102885743+LEONARDO-DATIOR@users.noreply.github.com>
2026-01-02 14:07:45 +0000

    Adicionando script para o botão de play

--- src/app/componentes/Generico/apresentacao/apresentacao.component.ts
+++ src/app/componentes/Generico/apresentacao/apresentacao.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { runInThisContext } from 'node:vm';
 
 @Component({
   selector: 'app-apresentacao',
@@ -6,5 +7,27 @@ import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
   templateUrl: './apresentacao.component.html',
 })
 export class ApresentacaoComponent {
-  
+  // EXIBIR VIDEO
+  @ViewChild('video', { static: false }) videoElementRef!: ElementRef;
+  @ViewChild('imagemBotaoPlay', { static: false })
+  botaoPlayElementRef!: ElementRef;
+
+  exibirVideo() {
+    this.botaoPlayElementRef.nativeElement.style.display = 'none';
+    this.videoElementRef.nativeElement.style.display = 'block';
+  }
+
+  imagemPlayApagado: string =
+    '../../../../assets/imagens/icons/botao_play_apagado.svg';
+  imagemPlayAcesso: string =
+    '../../../../assets/imagens/icons/botao_play_acesso.svg';
+  srcImagemHover: string = this.imagemPlayApagado;
+
+  onMouseEnter() {
+    this.srcImagemHover = this.imagemPlayAcesso;
+  }
+
+  onMouseLeave() {
+    this.srcImagemHover = this.imagemPlayApagado;
+  }
 }
