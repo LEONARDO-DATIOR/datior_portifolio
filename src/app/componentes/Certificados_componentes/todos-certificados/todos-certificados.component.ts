726d261204ff8d017b99714eab6148e89ed51e29
LEONARDO-DATIOR <leonardowederveiga@gmail.com>
2026-02-21 21:34:45 -0300

    Adicionando codigo para verificar se a plataforma é um browser antes de pegar os dados do csv, coreção deploy

--- src/app/componentes/Certificados_componentes/todos-certificados/todos-certificados.component.ts
+++ src/app/componentes/Certificados_componentes/todos-certificados/todos-certificados.component.ts
@@ -1,7 +1,7 @@
-import { Component, QueryList, ViewChildren } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, QueryList, ViewChildren } from '@angular/core';
 import { CertificadoComponent } from "../../componentesSmall/certificado/certificado.component";
 import { HttpClient } from '@angular/common/http';
-import { NgForOf } from '@angular/common';
+import { isPlatformBrowser, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-todos-certificados',
@@ -11,9 +11,11 @@ templateUrl: './todos-certificados.component.html',
 export class TodosCertificadosComponent {
   dadosCsv: any[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit() {
+    if (!isPlatformBrowser(this.platformId)) return;
+    
     this.http.get('assets/datas/CERTIFICADOS-DUCA.csv', { responseType: 'text' })
       .subscribe((textoCsv: string) => {
         const linhas: string[] = textoCsv
--- src/app/componentes/componentesSmall/certificado/certificado.component.ts
+++ src/app/componentes/componentesSmall/certificado/certificado.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, ViewChild, viewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BotaoPadraoComponent } from "../botao-padrao/botao-padrao.component";
 
 @Component({
