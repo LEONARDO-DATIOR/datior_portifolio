20f472a46848603f8852b488381dbdda745b0b7c
DUCA.LEONARDO <102885743+LEONARDO-DATIOR@users.noreply.github.com>
2026-07-29 14:22:02 +0000

    Ajustando o comportamento do Header na tela de celular

--- src/app/componentes/Generico/header/header.component.ts
+++ src/app/componentes/Generico/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,6 +7,29 @@ import { Router } from '@angular/router';
     templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+    // lógica para sumir ou aparecer com o menu de acordo com o click
+    @ViewChild('menu') menu!: ElementRef;
+    @ViewChild('burger_menu') burger_menu!: ElementRef;
+    menuAberto: boolean = false;
+
+    toggleMenu(event: MouseEvent): void {
+        event.stopPropagation();
+        this.menuAberto = !this.menuAberto;
+    }
+
+    @HostListener('document:click', ['$event'])
+    onDocumentClick(event: MouseEvent): void {
+        if (
+            this.menuAberto &&
+            this.menu &&
+            !this.menu.nativeElement.contains(event.target) &&
+            !this.burger_menu.nativeElement.contains(event.target)
+        ) {
+            this.menuAberto = false;
+        }
+    }
+
+    // Animação de sumir e aparece o header com o movimento do mouse
     estaAparecendo = false;
     scrollNoTopo = 0;
 
