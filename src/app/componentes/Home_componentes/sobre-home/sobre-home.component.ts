487d990852e25063b240ba41d2de15473b67b2a1
DUCA.LEONARDO <102885743+LEONARDO-DATIOR@users.noreply.github.com>
2026-07-29 19:32:25 +0000

    Ajuste na apresentação visual inicial da home

--- src/app/componentes/Home_componentes/sobre-home/sobre-home.component.ts
+++ src/app/componentes/Home_componentes/sobre-home/sobre-home.component.ts
@@ -1,44 +1,49 @@
-import { Component, ElementRef, OnInit, ViewChild, NgZone} from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    OnInit,
+    ViewChild,
+    NgZone,
+} from '@angular/core';
 import { BotaoPadraoComponent } from '../../componentesSmall/botao-padrao/botao-padrao.component';
 
 @Component({
-  selector: 'app-sobre-home',
-  imports: [BotaoPadraoComponent],
-  templateUrl: './sobre-home.component.html',
+    selector: 'app-sobre-home',
+    imports: [BotaoPadraoComponent],
+    templateUrl: './sobre-home.component.html',
 })
+export class SobreHomeComponent {
+    textoDigitado = '';
+    textoCompleto = `O objetivo deste portfólio vai além de simplesmente me apresentar para o mercado de trabalho. Na verdade, ele nasce do mesmo motivo pelo qual criamos álbuns de fotos. Não fazemos álbuns apenas para mostrar imagens a outras pessoas, mas para registrar conquistas, guardar momentos felizes e preservar memórias. Eles existem tanto para serem compartilhados quanto para que nós mesmos possamos revisitar nossa história e enxergar a nossa própria evolução. Este site é minha arte. Um espaço onde registro tudo o que conquistei, como evoluí ao longo do tempo e do que sou capaz.`;
 
-export class SobreHomeComponent  {
-  textoDigitado = '';
-  textoCompleto = `O objetivo deste portfólio vai além de simplesmente me apresentar para o mercado de trabalho. Na verdade, ele nasce do mesmo motivo pelo qual criamos álbuns de fotos. Não fazemos álbuns apenas para mostrar imagens a outras pessoas, mas para registrar conquistas, guardar momentos felizes e preservar memórias. Eles existem tanto para serem compartilhados quanto para que nós mesmos possamos revisitar nossa história e enxergar a nossa própria evolução. Este site é minha arte. Um espaço onde registro tudo o que conquistei, como evoluí ao longo do tempo e do que sou capaz.`;
+    @ViewChild('sectionRef', { static: true }) sectionRef!: ElementRef;
+    jaDigitou = false;
 
-  @ViewChild('sectionRef', { static: true }) sectionRef!: ElementRef;
-  jaDigitou = false;
+    constructor(private ngZone: NgZone) {}
 
-  constructor(private ngZone: NgZone) {}
+    ngAfterViewInit(): void {
+        requestAnimationFrame(() => {
+            const observer = new IntersectionObserver(
+                ([entry]) => {
+                    if (entry.isIntersecting && !this.jaDigitou) {
+                        this.jaDigitou = true;
+                        this.digitarTexto();
+                        observer.disconnect();
+                    }
+                },
+                { threshold: 0.5 },
+            );
 
-  ngAfterViewInit(): void {
-    requestAnimationFrame(() => {
-      const observer = new IntersectionObserver(
-        ([entry]) => {
-          if (entry.isIntersecting && !this.jaDigitou) {
-            this.jaDigitou = true;
-            this.digitarTexto();
-            observer.disconnect();
-          }
-        },
-        { threshold: 0.5 }
-      );
-
-      observer.observe(this.sectionRef.nativeElement);
-    });
-  }
-
-  digitarTexto(index = 0) {
-    if (index < this.textoCompleto.length) {
-      this.ngZone.run(() => {
-        this.textoDigitado += this.textoCompleto.charAt(index);
-      });
-      setTimeout(() => this.digitarTexto(index + 1), 25);
+            observer.observe(this.sectionRef.nativeElement);
+        });
     }
-  }
+
+    digitarTexto(index = 0) {
+        if (index < this.textoCompleto.length) {
+            this.ngZone.run(() => {
+                this.textoDigitado += this.textoCompleto.charAt(index);
+            });
+            setTimeout(() => this.digitarTexto(index + 1), 25);
+        }
+    }
 }
