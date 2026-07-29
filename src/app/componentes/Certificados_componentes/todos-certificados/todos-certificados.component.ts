503877da0c7c68a4caf62d2e9b8768916292a578
DUCA.LEONARDO <102885743+LEONARDO-DATIOR@users.noreply.github.com>
2026-07-29 17:45:25 +0000

    Adicionando cor para cada categoria de certificado

--- src/app/componentes/Certificados_componentes/todos-certificados/todos-certificados.component.ts
+++ src/app/componentes/Certificados_componentes/todos-certificados/todos-certificados.component.ts
@@ -1,33 +1,40 @@
-import { Component, Inject, PLATFORM_ID, QueryList, ViewChildren } from '@angular/core';
-import { CertificadoComponent } from "../../componentesSmall/certificado/certificado.component";
+import {
+    Component,
+    Inject,
+    PLATFORM_ID,
+    QueryList,
+    ViewChildren,
+} from '@angular/core';
+import { CertificadoComponent } from '../../componentesSmall/certificado/certificado.component';
 import { HttpClient } from '@angular/common/http';
 import { isPlatformBrowser, NgForOf } from '@angular/common';
-import * as Papa from 'papaparse';  
+import * as Papa from 'papaparse';
 
 @Component({
-  selector: 'app-todos-certificados',
-  imports: [CertificadoComponent, NgForOf],
-templateUrl: './todos-certificados.component.html',
+    selector: 'app-todos-certificados',
+    imports: [CertificadoComponent, NgForOf],
+    templateUrl: './todos-certificados.component.html',
 })
 export class TodosCertificadosComponent {
-  dadosCsv: any[] = [];
+    dadosCsv: any[] = [];
 
-  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {}
+    constructor(
+        private http: HttpClient,
+        @Inject(PLATFORM_ID) private platformId: Object,
+    ) {}
 
-  ngOnInit() {
-    if (!isPlatformBrowser(this.platformId)) return;
-    
-    this.http
-      .get('assets/datas/CERTIFICADOS-DUCA.csv', { responseType: 'text' })
-      .subscribe((textoCsv: string) => {
+    ngOnInit() {
+        if (!isPlatformBrowser(this.platformId)) return;
 
-        const resultado = Papa.parse(textoCsv, {
-          header: true,
-          skipEmptyLines: true
-        });
+        this.http
+            .get('assets/datas/CERTIFICADOS-DUCA.csv', { responseType: 'text' })
+            .subscribe((textoCsv: string) => {
+                const resultado = Papa.parse(textoCsv, {
+                    header: true,
+                    skipEmptyLines: true,
+                });
 
-        this.dadosCsv = resultado.data;
-      });
-  }
-
+                this.dadosCsv = resultado.data;
+            });
+    }
 }
--- src/app/componentes/componentesSmall/certificado/certificado.component.ts
+++ src/app/componentes/componentesSmall/certificado/certificado.component.ts
@@ -1,14 +1,41 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-certificado',
-  templateUrl: './certificado.component.html',
+    selector: 'app-certificado',
+    templateUrl: './certificado.component.html',
+    imports: [NgClass],
 })
 export class CertificadoComponent {
-  @Input() link: string = '#';
-  @Input() titulo: string = 'SEM NOME';
-  @Input() escola: string = '';
-  @Input() dataRealizacao: string = '';
-  @Input() categoria: string = 'Não categorizado';
+    @Input() link: string = '#';
+    @Input() titulo: string = 'SEM NOME';
+    @Input() escola: string = '';
+    @Input() dataRealizacao: string = '';
+    @Input() categoria: string = 'Não categorizado';
 
+    // Mudar classe do button de acordo com a categoria vinda do CSV:
+    getClasseCategoria(categoria: string): string {
+        switch (categoria) {
+            case 'FRONT-END':
+                return 'front-end';
+            case 'BACK-END':
+                return 'back-end';
+            case 'PROGRAMAÇÃO / ALGORITMO':
+                return 'back-end';
+            case 'BANCO DE DADOS':
+                return 'back-end';
+            case 'UI/UX':
+                return 'design';
+            case 'ANALISE DE DADOS':
+                return 'analise-de-dados';
+            case 'IA':
+                return 'analise-de-dados';
+            case 'REDES/CLOUD':
+                return 'infraestrutura';
+            case 'METODOLOGIAS AGEIS':
+                return 'gestao';
+            default:
+                return 'sem-categoria';
+        }
+    }
 }
