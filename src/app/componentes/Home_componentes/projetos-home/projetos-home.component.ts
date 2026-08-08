d1995d468d9e334eece094a0130c412a1c4b9900
DUCA.LEONARDO <102885743+LEONARDO-DATIOR@users.noreply.github.com>
2026-08-08 21:53:51 +0000

    :👌: improvement(Lista projetos) Melhorando a listagem de projetos

--- src/app/componentes/Home_componentes/projetos-home/projetos-home.component.ts
+++ src/app/componentes/Home_componentes/projetos-home/projetos-home.component.ts
@@ -2,9 +2,29 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ProjetoDivComponent } from '../../componentesSmall/projeto-div/projeto-div.component';
 
 @Component({
-  selector: 'app-projetos-home',
-  imports: [ProjetoDivComponent],
-  templateUrl: './projetos-home.component.html',
+    selector: 'app-projetos-home',
+    imports: [ProjetoDivComponent],
+    templateUrl: './projetos-home.component.html',
 })
 export class ProjetosHomeComponent {
+    projetos = [
+        {
+            titulo: 'Datior Home',
+            descricao:
+                'Este é o projeto que você está vendo agora. Ele é o meu portfólio pessoal, onde compartilho minha trajetória, conquistas e habilidades.',
+            caminho_imagem:
+                '/../../../assets/imagens/projetos/projeto_portifolio.png',
+            link_github: 'https://github.com/LEONARDO-DATIOR/datior_portifolio',
+            tecnologias: ['Angular', 'HTML', 'SCSS', 'TypeScript'],
+        },
+        {
+            titulo: 'Overdrive-Info',
+            descricao:
+                'Projeto desenvolvido para monitoramento e diagnóstico de equipamentos, com coleta e análise de informações de hardware e sistema. A solução permite identificar falhas, anomalias e possíveis gargalos de desempenho, além de gerar relatórios técnicos para auxiliar na análise e manutenção das máquinas.',
+            caminho_imagem:
+                '/../../../assets/imagens/projetos/Overdrive-info.png',
+            link_github: 'https://github.com/LEONARDO-DATIOR/datior_portifolio',
+            tecnologias: ['Python', 'Playwright'],
+        },
+    ];
 }
